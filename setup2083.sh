7547d3cf774b41cefd11db76cd2aab160112b4c6
fa-kfmt <100280007+fa-kfmt@users.noreply.github.com>
2022-05-19 17:22:47 +0700

    Update setup2083.sh

--- setup2083.sh
+++ setup2083.sh
@@ -91,7 +91,7 @@ rvm use 3.0.2 --default
 git clone https://gitlab.com/fa-kfmt/chatwoot.git
 cd chatwoot
 if [[ -z "$1" ]]; then
-git checkout port2083;
+git checkout main;
 else
 git checkout $1;
 fi
